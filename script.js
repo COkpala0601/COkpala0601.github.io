d5d5035d322326a7f7d02606f5b9759af92eac9b
COkpala0601 <31930972+COkpala0601@users.noreply.github.com>
2017-09-13 16:17:44 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+var a = 1;
+var b = 2;
+var c = 3;
+var d = 4;
+var e = 5;
+var f = 6;
+var g = 7;
+var h = 8;
+var i = 9;
+var j = 10;
+var k = 11;
+var l = 12;
+var m = 13;
+
+var ad = a+b+c+d+e+f+g+h+i+j+k+l+m;
+alert(ad);

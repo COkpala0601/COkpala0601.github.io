635e40f15aeefe777c5e8191fcca5944e9732faa
COkpala0601 <31930972+COkpala0601@users.noreply.github.com>
2017-09-13 19:00:54 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+var riddle = prompt("What shape has 13 sides");
+
+if(riddle === 	triskaidecagon || tridecagon){
+  alert("access granted");
+}  
+else{
+ alert("acces denied");
+  prompt("What shape has 13 sides");
+} 

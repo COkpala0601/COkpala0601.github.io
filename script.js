c5ce630e5c51c87da15d46494a38fac478aac4a7
COkpala0601 <31930972+COkpala0601@users.noreply.github.com>
2017-09-13 18:58:45 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,9 @@
-var a = 1;
-var b = 2;
-var c = 3;
-var d = 4;
-var e = 5;
-var f = 6;
-var g = 7;
-var h = 8;
-var i = 9;
-var j = 10;
-var k = 11;
-var l = 12;
-var m = 13;
+var riddle = prompt("What shape has 13 sides");
 
-var ad = a+b+c+d+e+f+g+h+i+j+k+l+m;
-alert(ad);
+if(riddle === 	triskaidecagon || tridecagon){
+  alert("access granted");
+}  
+else{
+ alert("acces denied");
+  riddle;
+} 
